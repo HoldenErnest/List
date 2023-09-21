fe2aac0e2f421c67d7a56ab792511e2944fd163f
Holden Ernest <holden.ernest@icloud.com>
2023-09-21 12:40:29 -0400

    most likely not working, trying to get export modules working

---
+++ listItem.js
@@ -0,0 +1,10 @@
+export class ListItem {
+    title;
+    description;
+    rating;
+    constructor() {
+        title = "unknown title";
+        description = "unknown description";
+        rating = 9;
+    }
+}
--- main.js
+++ main.js
@@ -1,4 +1,6 @@
+import li from "./listItem.js"
 
+var theList = [2];
 
 function newEntry() {
     var isHidden = document.getElementById("create").style.display == "none"
@@ -6,4 +8,31 @@ function newEntry() {
         document.getElementById("create").style.display = "block";
     else
     document.getElementById("create").style.display = "none";
+}
+
+function sortBy(value) {
+    
+    switch (value){
+        case '1':
+            console.log("sort by title");
+            break;
+        case '2':
+            console.log("sort by rating");
+            break;
+        case '3':
+            console.log("sort by other");
+            break;
+        default:
+            console.log("not a choice");
+            break;
+    }
+}
+function addListItem() {
+    theList.push(1);
+    var eDiv = document.getElementById("allEntries");
+    eDiv.setAttribute("style","height:"+ (theList.length * 30) +"px");
+}
+
+function toListItem(item) {
+    var newItem = new li.ListItem();
 }
