c4da73c6e377651a57db92eb55940c22a9c97055
HoldenErnest <holden.ernest@icloud.com>
2023-10-10 20:20:49 -0400

    scrapped the base garbage

--- ListApp/src/app/app.component.ts
+++ ListApp/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+let someWord = "<placeholder words>";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'ListApp';
+  name = someWord;
 }
