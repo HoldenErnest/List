d1f7c1019e30e247267a47013b13b17ab8c0c5ec
HoldenErnest <holden.ernest@icloud.com>
2024-01-14 13:16:58 -0500

    lists can now be loaded and displayed

--- renderer.js
+++ renderer.js
@@ -7,12 +7,10 @@ const path = require('node:path');
 
 
 const saveBtn = document.getElementById("save");
-const exitBtn = document.getElementById("exit");
 const loadListBtn = document.getElementById("load-list");
 
 //Event listeners
 saveBtn.addEventListener('click', onButtonSave);
-exitBtn.addEventListener('click', onButtonExit);
 loadListBtn.addEventListener('click', retrieveList);
 
 function onButtonSave() {
@@ -26,13 +24,36 @@ function retrieveList() { // this will be later loaded from a server instead of 
     fs.readFile(path.join(__dirname, 'templist.csv'), 'utf8', function (err, data) {
         if (err) return console.error(err);
         // data is the contents of the text file we just read
-        console.log(data);
         var listArray = parseToArray(data,',');
-        console.log(listArray);
+        console.log('loading ' + listArray.length + ' items to the list');
+        displayListItems(listArray);
         // I guess just use in the async function?
         // load each list item here
     });
 
+}
+
+function displayListItems(listData) {
+    let itemCount = document.querySelectorAll('.item').length + 1;
+    for(let i = 0; i < listData.length; i++) {
+        displayListItem(listData[i], itemCount+i);
+    }
+    // TODO: sort by whatever, then display, maybe in a displaySorted(sortby)
+}
+function displayListItem(itemData, itemID) {
+    var original = document.getElementById('tempListItem');
+    if (original == null) return;
+    var clone = original.cloneNode(true); // "deep" clone
+    clone.id = '';
+    // set all of these clones child divs to use the listItem information
+    clone.getElementsByClassName("item-id")[0].innerHTML = itemID | document.querySelectorAll('.item').length + 1; // probably better to do this query once before the for loop earlier
+    clone.getElementsByClassName("item-title")[0].innerHTML = itemData.title;
+    clone.getElementsByClassName("item-tags")[0].innerHTML = itemData.tags;
+    clone.getElementsByClassName("item-rating")[0].innerHTML = itemData.rating;
+
+
+    //clone.onclick = duplicate; // might need to add listeners to this clone
+    original.parentNode.appendChild(clone);
 }
 
 function parseToArray(stringVal, splitter) {
