ffde6a9733e80cd68559cca7c09dbebd563df931
HoldenErnest <holden.ernest@icloud.com>
2024-01-22 17:20:58 -0500

    login screen prettied

--- login.js
+++ login.js
@@ -4,7 +4,6 @@
 let login = document.getElementById('login');
 let loginUser = document.getElementById('loginUser');
 let loginPass = document.getElementById('loginPass');
-console.log(login);
 login.addEventListener('click', () => {
     window.api.send('attempt-login', {user: loginUser.value, pass: loginPass.value});
 });
